c7a73fecfb38f629aed13b843e06c058708559e9
yamamoto-yuto <yy_black200296@icloud.com>
2026-03-02 05:44:14 +0900

    バリデーションの実装

--- src/main/java/com/example/foodapp/form/ProductionForm.java
+++ src/main/java/com/example/foodapp/form/ProductionForm.java
@@ -13,11 +13,7 @@ public class ProductionForm {
 	@NotNull(message = "商品を選択してください")
 	private Long productId;
 
-	@NotNull(message = "日付を入力してください")
-	private LocalDate productionDate;
-
-	@NotNull(message = "数量を入力してください")
-	@Min(value = 1, message = "数量は1以上で入力してください")
+	@NotNull(message = "製造数量を入力してください")
+	@Min(value = 1, message = "製造数量は1以上で入力してください")
 	private Integer quantity;
-
 }
